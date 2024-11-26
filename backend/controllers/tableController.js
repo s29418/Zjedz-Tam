9d5981ce98c9f024ed428e78319d2b8189406b46
s29418 <s29418@pjwstk.edu.pl>
2024-11-26 14:39:43 +0100

    Added tableController class.

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -7,7 +7,7 @@ exports.getTables = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Błąd serwera' });
     }
-}
+};
 
 exports.getTable = async (req, res) => {
     try {
@@ -20,5 +20,37 @@ exports.getTable = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Błąd serwera' });
+    }
+};
+
+exports.addTable = async (req, res) => {
+    try{
+        const restaurantId = req.params.id;
+        const seats = req.body;
+        await db.query('INSERT INTO Restauranttable (seats) VALUES ? WHERE restauran_id = ?', [seats, restaurantId]);
+        res.status(201).json({ message: 'Dodano stolik' });
+    } catch (error) {
+        res.status(500).json({ error: 'Błąd serwera.'});
+    }
+};
+
+exports.updateRestaurant = async (req, res) => {
+    try{
+        const tableId = req.params.id;
+        const seats = req.body;
+        await db.query('UPDATE Restauranttable SET seats = ? WHERE table_id = ?', [seats, tableId]);
+        res.status(204).json({ message: 'Zaktualizowano informacje na temat stolika' });
+    } catch (error) {
+        res.status(500).json({ error: 'Błąd serwera.'});
+    }
+};
+
+exports.deleteRestaurant = async (req, res) => {
+    try{
+        const tableId = req.params.id;
+        await db.query('DELETE FROM Restauranttable WHERE table_id = ?', [tableId]);
+        res.status(204).json({ message: 'Usunięto stolik z restauracji' });
+    } catch (error) {
+        res.status(500).json({ error: 'Błąd serwera.'});
     }
 }

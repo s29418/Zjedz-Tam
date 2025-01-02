4a3836813623167b14d635d9731d742489f2c9d3
s29418 <s29418@pjwstk.edu.pl>
2025-01-02 18:49:33 +0100

    Added opening hours on Restaurant Page

--- frontend/src/pages/RestaurantDetails.js
+++ frontend/src/pages/RestaurantDetails.js
@@ -19,16 +19,46 @@ function RestaurantDetails() {
         <div>
             <div className="restaurantcontent">
 
-                <div className="restaurantimage">
-                    <img
-                         src={restaurant.image ? `/images/${restaurant.image}` : `/images/no-image.jpg`}
-                         alt={restaurant.name}
-                    />
+                <div className="left-panel">
+
+                    <div className="restaurantimage">
+                        <img
+                            src={restaurant.image ? `/images/${restaurant.image}` : `/images/no-image.jpg`}
+                            alt={restaurant.name}
+                        />
+                    </div>
+
+                    <div className="opening-hours">
+                        <h2>Godziny otwarcia:</h2>
+
+                        {restaurant.opening_hours ? (
+                            <table className="menu-table">
+                                <tbody>
+                                <tr>
+                                    <th>Dzień</th>
+                                    <th>Godziny</th>
+                                </tr>
+                                </tbody>
+
+                                {Object.entries(restaurant.opening_hours).map(([day, hours]) => (
+                                    <tr key={day}>
+                                        <td>{day}</td>
+                                        <td>{hours}</td>
+                                    </tr>
+                                ))}
+
+
+                            </table>
+                        ) : (
+                            <p>Brak danych</p>
+                        )}
+
+                    </div>
                 </div>
 
                 <div className="text-content">
 
-                    <h1 class="restaurantname">{restaurant.name}</h1>
+                    <h1 className="restaurantname">{restaurant.name}</h1>
                     <hr className="restaurantunderline"/>
                     <p className="description">{restaurant.description}</p>
 
